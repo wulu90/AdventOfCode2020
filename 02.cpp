5d42b52b39d1b97cb177615cd17d18d49ce20a4f
wulu <wulu90@outlook.com>
2024-04-10 15:38:53 +0800

    day02

---
+++ 02.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <ranges>
+#include <string_view>
+#include <string>
+#include <charconv>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+struct pass
+{
+    int lowest;
+    int highest;
+    char letter;
+    string str;
+    bool valid;
+
+    pass(const string &s) : valid{false}
+    {
+        auto inx = s.find('-');
+        from_chars(&s[0], &s[inx], lowest);
+        auto sp = s.find(' ');
+        from_chars(&s[inx + 1], &s[sp], highest);
+
+        letter = s[sp + 1];
+        str = s.substr(sp + 4);
+
+        auto n = count(str.begin(), str.end(), letter);
+        if (n >= lowest && n <= highest)
+        {
+            valid = true;
+        }
+    }
+
+    bool valid2()
+    {
+        if ((str[lowest - 1] == letter && str[highest - 1] != letter) || (str[lowest - 1] != letter && str[highest - 1] == letter))
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+void part1()
+{
+    ifstream input("input");
+    string line;
+    int count = 0;
+    while (getline(input, line))
+    {
+        pass p(line);
+        if (p.valid)
+        {
+            ++count;
+        }
+    }
+
+    cout << count << '\n';
+}
+
+void part2()
+{
+    ifstream input("input");
+    string line;
+    int count = 0;
+    while (getline(input, line))
+    {
+        pass p(line);
+        if (p.valid2())
+        {
+            ++count;
+        }
+    }
+
+    cout << count << '\n';
+}
+
+int main()
+{
+    part1();
+    part2();
+}
