fe688db3a63ffd5399c5e6b25ab82975ef274605
wulu <wulu90@outlook.com>
2024-04-10 11:45:59 +0800

    day1

--- 1.cpp
+++ 1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <set>
+#include <vector>
 
 using namespace std;
 
-int main()
+void part1()
 {
     ifstream input("input");
     set<int16_t> inset;
@@ -14,7 +15,7 @@ int main()
     {
         if (inset.contains(2020 - i))
         {
-            cout << i * (2020 - i);
+            cout << i * (2020 - i) << '\n';
             break;
         }
         else
@@ -22,5 +23,47 @@ int main()
             inset.insert(i);
         }
     }
-    return 0;
+}
+
+void part2()
+{
+    ifstream input("input");
+    set<int16_t> inset;
+    vector<int16_t> invec;
+
+    int16_t in;
+    while (input >> in)
+    {
+        inset.insert(in);
+        invec.push_back(in);
+    }
+
+    for (size_t i = 0; i < invec.size(); ++i)
+    {
+        bool find = false;
+        for (size_t j = i; j < invec.size() - 1; ++j)
+        {
+            auto m = 2020 - invec[i] - invec[j];
+            if (m == 0)
+            {
+                continue;
+            }
+            if (inset.contains(m))
+            {
+                cout << invec[i] * invec[j] * m << '\n';
+                find = true;
+                break;
+            }
+        }
+        if (find)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    part1();
+    part2();
 }
