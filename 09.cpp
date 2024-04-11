2530e28e8e2ef95add2909d9da433395a19e81f6
wulu <wulu90@outlook.com>
2024-04-11 16:51:05 +0800

    day09 p2

--- 09.cpp
+++ 09.cpp
@@ -1,5 +1,7 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <set>
 #include <string>
 #include <vector>
@@ -41,7 +43,41 @@ void part1() {
     }
 }
 
+void part2() {
+    ifstream input("input");
+    vector<int64_t> xmas;
+    for (int64_t t; input >> t;) {
+        xmas.push_back(t);
+    }
+
+    int64_t weakness = 1930745883;
+
+    vector<int64_t> sumvec;
+    map<int64_t, size_t> sum_index;
+
+    int64_t sum = 0;
+    for (size_t i = 0; i < xmas.size(); ++i) {
+        sum += xmas[i];
+        sumvec.push_back(sum);
+        sum_index.insert({sum, i});
+    }
+
+    size_t l = 0;
+    size_t r = 0;
+    for (size_t i = 0; i < xmas.size(); ++i) {
+        if (sum_index.contains(sumvec[i] - weakness)) {
+            l = sum_index[sumvec[i] - weakness];
+            r = i;
+            break;
+        }
+    }
+
+    auto [min, max] = minmax_element(xmas.begin() + l + 1, xmas.begin() + r + 1);
+    cout << *min + *max << '\n';
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
