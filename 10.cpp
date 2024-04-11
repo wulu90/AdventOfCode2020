3ab7330793c36057a2baf0be72e2b9207d553b80
wulu <wulu90@outlook.com>
2024-04-11 20:51:06 +0800

    day10 p2

--- 10.cpp
+++ 10.cpp
@@ -27,7 +27,32 @@ void part1() {
     cout << diff_num[1] * diff_num[3] << '\n';
 }
 
+void part2() {
+    ifstream input("input");
+    vector<int> adaptervec;
+    for (int i = 0; input >> i;) {
+        adaptervec.push_back(i);
+    }
+
+    sort(adaptervec.begin(), adaptervec.end(), greater<int>());
+
+    vector<int64_t> dp(adaptervec.size() + 1, 0l);
+
+    dp.front() = 1;
+
+    for (size_t i = 0; i < dp.size(); ++i) {
+        for (size_t j = 1; j <= 3; ++j) {
+            if (adaptervec[i + j] >= adaptervec[i] - 3) {
+                dp[i + j] += dp[i];
+            }
+        }
+    }
+
+    cout << dp.back() << '\n';
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
